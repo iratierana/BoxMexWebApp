7197e50fdbd1f6a85480cfd5afc5bad9dc2df629
xabier <xabier.jauregi@alumni.mondragon.edu>
2017-05-16 09:34:48 +0200

    Robot info controller

--- src/actions/robota/RobotInfoIkusi.java
+++ src/actions/robota/RobotInfoIkusi.java
@@ -1,51 +1,71 @@
 package actions.robota;
 
-import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import com.opensymphony.xwork2.ActionContext;
 
+import databaseConn.DatabaseConnect;
 import entitys.system.Robot;
+import entitys.system.Usuario;
 
 public class RobotInfoIkusi {
-		
+	
+	Map<String, Object> session;
 	Robot robotIrtera = null;
 	Robot robotSarrera = null;
 	
 	public String execute(){
 				
-		robotIrtera = webZerbitzuaDeituEtaRobotaKargatu(1, "entrada");
-		robotIrtera = webZerbitzuaDeituEtaRobotaKargatu(2, "salida");
+		robotSarrera = robotarenInformazioaIkusi(2, "entrada");
+		robotIrtera = robotarenInformazioaIkusi(1, "salida");
 		
 		return "goToRobotInfo";
 	}
 	
-	private Robot webZerbitzuaDeituEtaRobotaKargatu(int robotId, String tipo){
+	private Robot robotarenInformazioaIkusi(int robotId, String tipo){
 		
-		Client client = null;
-		Robot robot = null;		
+		Robot robot = null;
+		Usuario loggedUser = null;
 		
-		try {
-			
-			client = Client.create();
-			WebResource webResource = client.resource("http://localhost:8080/BoxMexWebApp/robotInfo/"+robotId+"/"+tipo);
-			ClientResponse response = webResource.accept(MediaType.TEXT_PLAIN).get(ClientResponse.class);
-			
-			if (response.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-			}			
-			
-			String result = response.getEntity(String.class);
-			System.out.println("Robot "+robotId+": \n"+result);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			client.destroy();
+		session = ActionContext.getContext().getSession();
+		loggedUser = (Usuario) session.get("loggedUser");
+		
+		switch (loggedUser.getTipoUsuario()) {
+		case "A":
+			DatabaseConnect.connectToTheDatabase("localhost", 5432, "BoxMexDatabase", "boxmexadmin", "1234");
+			robot = infoRobotConsulta(robotId, tipo);
+			break;
+		case "P":
+			DatabaseConnect.connectToTheDatabase("localhost", 5432, "BoxMexDatabase", "boxmexpeon", "1234");
+			robot = infoRobotConsulta(robotId, tipo);
+			break;
 		}
 		
 		return robot;
+	}
+	
+	private Robot infoRobotConsulta(int robotId, String tipo){
+		
+		Robot robot = null;
+		try {
+			PreparedStatement statement = DatabaseConnect.conn.prepareStatement("SELECT * FROM boxmexsystem.robot WHERE robotid = ? AND tiporobot = ?");
+			statement.setInt(1, robotId);
+			statement.setString(2, tipo);
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()){
+				robot = new Robot(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4));
+			}
+		} catch (SQLException e) {
+			DatabaseConnect.disconnectToFromTheDatabase();
+			e.printStackTrace();
+		}
+		
+		DatabaseConnect.disconnectToFromTheDatabase();
+		return robot;
+		
 	}
 
  	public Robot getRobotIrtera() {
