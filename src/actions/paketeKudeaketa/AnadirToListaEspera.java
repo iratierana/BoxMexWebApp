41e014c01adcddb862cafbf57f598775444853d9
xabier <xabier.jauregi@alumni.mondragon.edu>
2017-05-22 14:27:23 +0200

    check style corrections

--- src/actions/paketeKudeaketa/AnadirToListaEspera.java
+++ src/actions/paketeKudeaketa/AnadirToListaEspera.java
@@ -7,52 +7,87 @@ import databaseConn.DatabaseConnect;
 import entitys.system.Pakete;
 import varGlobal.VariablesGlobales;
 
+/**
+ * The Class AnadirToListaEspera.
+ */
 public class AnadirToListaEspera {
-	
-	ArrayList<Pakete> listPakeEntrada;	
+
+	/** The Constant PORT. */
+	private static final int PORT = 5432;
+
+	/** The list paketes de entrada. */
+	ArrayList<Pakete> listPakeEntrada;
+
+	/** The id list pakete entrada. */
 	ArrayList<Integer> idListPakeEntrada;
+
+	/** The pakete select. */
 	int paketeSelect;
-	
-	public String execute(){
-		DatabaseConnect.connectToTheDatabase("localhost", 5432, "BoxMexDatabase", "boxmexadmin", "1234");
+
+	/**
+	 * Adds the selected paket to sleep list in order to get with a web servie.
+	 *
+	 * @return the string
+	 */
+	public String execute() {
+		DatabaseConnect.connectToTheDatabase("localhost", PORT, "BoxMexDatabase", "boxmexadmin", "1234");
 		Pakete pakete = PaketeDAO.cargarPakete(paketeSelect);
 		VariablesGlobales.listaPakEnEsperaParaIA.add(pakete);
 		PaketeDAO.cambiarEstadoToPakete(paketeSelect, "quieto");
 		listPakeEntrada = PaketeDAO.actualizarListaPaquetesEstrada();
-		
+
 		DatabaseConnect.disconnectToFromTheDatabase();
 		idListPakeEntradaBete();
 		return "ok";
 	}
-	
-	
-	private void idListPakeEntradaBete(){
-		idListPakeEntrada= new ArrayList<Integer>();
-		for(Pakete p : listPakeEntrada){
+
+	/**
+	 * Id list pake entrada bete.
+	 */
+	private void idListPakeEntradaBete() {
+		idListPakeEntrada = new ArrayList<Integer>();
+		for (Pakete p : listPakeEntrada) {
 			idListPakeEntrada.add(p.getId());
 		}
-	
+
 	}
-	
+
+	/**
+	 * Gets the id list pake entrada.
+	 *
+	 * @return the id list pake entrada
+	 */
 	public ArrayList<Integer> getIdListPakeEntrada() {
 		return idListPakeEntrada;
 	}
 
-
-	public void setIdListPakeEntrada(ArrayList<Integer> idListPakeEntrada) {
+	/**
+	 * Sets the id list pake entrada.
+	 *
+	 * @param idListPakeEntrada
+	 *            the new id list pake entrada
+	 */
+	public void setIdListPakeEntrada(final ArrayList<Integer> idListPakeEntrada) {
 		this.idListPakeEntrada = idListPakeEntrada;
 	}
 
-
+	/**
+	 * Gets the pakete select.
+	 *
+	 * @return the pakete select
+	 */
 	public int getPaketeSelect() {
 		return paketeSelect;
 	}
 
-
-	public void setPaketeSelect(int paketeSelect) {
+	/**
+	 * Sets the pakete select.
+	 *
+	 * @param paketeSelect
+	 *            the new pakete select
+	 */
+	public void setPaketeSelect(final int paketeSelect) {
 		this.paketeSelect = paketeSelect;
 	}
-	
-	
 
 }
--- src/actions/paketeKudeaketa/GotoPaketeKudeaketa.java
+++ src/actions/paketeKudeaketa/GotoPaketeKudeaketa.java
@@ -6,48 +6,79 @@ import DAO.PaketeDAO;
 import databaseConn.DatabaseConnect;
 import entitys.system.Pakete;
 
+/**
+ * The Class GotoPaketeKudeaketa.
+ */
 public class GotoPaketeKudeaketa {
-	
-	ArrayList<Pakete> listPakeEntrada;	
+
+	/** The Constant PORT. */
+	private static final int PORT = 5432;
+
+	/** The list pake entrada. */
+	ArrayList<Pakete> listPakeEntrada;
+
+	/** The id list pake entrada. */
 	ArrayList<Integer> idListPakeEntrada;
-	
-	public String execute(){
-		DatabaseConnect.connectToTheDatabase("localhost", 5432, "BoxMexDatabase", "boxmexadmin", "1234");
+
+	/**
+	 * Execute.
+	 *
+	 * @return the string
+	 */
+	public String execute() {
+		DatabaseConnect.connectToTheDatabase("localhost", PORT, "BoxMexDatabase", "boxmexadmin", "1234");
 		listPakeEntrada = PaketeDAO.actualizarListaPaquetesEstrada();
 		DatabaseConnect.disconnectToFromTheDatabase();
 		idListPakeEntradaBete();
 		return "goToPaketeKudeaketa";
 	}
-	
-	
-	private void idListPakeEntradaBete(){
-		idListPakeEntrada= new ArrayList<Integer>();
-		for(Pakete p : listPakeEntrada){
+
+	/**
+	 * Id list pake entrada bete.
+	 */
+	private void idListPakeEntradaBete() {
+		idListPakeEntrada = new ArrayList<Integer>();
+		for (Pakete p : listPakeEntrada) {
 			idListPakeEntrada.add(p.getId());
 		}
 	}
 
-
+	/**
+	 * Gets the list pake entrada.
+	 *
+	 * @return the list pake entrada
+	 */
 	public ArrayList<Pakete> getListPakeEntrada() {
 		return listPakeEntrada;
 	}
 
-
-	public void setListPakeEntrada(ArrayList<Pakete> listPakeEntrada) {
+	/**
+	 * Sets the list pake entrada.
+	 *
+	 * @param listPakeEntrada
+	 *            the new list pake entrada
+	 */
+	public void setListPakeEntrada(final ArrayList<Pakete> listPakeEntrada) {
 		this.listPakeEntrada = listPakeEntrada;
 	}
 
-
+	/**
+	 * Gets the id list pake entrada.
+	 *
+	 * @return the id list pake entrada
+	 */
 	public ArrayList<Integer> getIdListPakeEntrada() {
 		return idListPakeEntrada;
 	}
 
-
-	public void setIdListPakeEntrada(ArrayList<Integer> idListPakeEntrada) {
+	/**
+	 * Sets the id list pake entrada.
+	 *
+	 * @param idListPakeEntrada
+	 *            the new id list pake entrada
+	 */
+	public void setIdListPakeEntrada(final ArrayList<Integer> idListPakeEntrada) {
 		this.idListPakeEntrada = idListPakeEntrada;
 	}
 
-
-
-	
 }
