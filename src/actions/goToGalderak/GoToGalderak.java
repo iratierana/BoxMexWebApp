fecb6d11556aed0a01ecc750a6af3047cf1db0f7
xabier <xabier.jauregi@alumni.mondragon.edu>
2017-05-30 12:37:00 +0200

    connect to questions

---
+++ src/actions/goToGalderak/GoToGalderak.java
@@ -0,0 +1,16 @@
+package actions.goToGalderak;
+
+/**
+ * The Class GoToGalderak.
+ */
+public class GoToGalderak {
+
+	/**
+	 * Execute.
+	 *
+	 * @return the string
+	 */
+	public String execute() {
+		return "ok";
+	}
+}
